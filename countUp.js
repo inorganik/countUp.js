401c4a929f48053140cad2f1a08b3cc2a3a9c876
inorganik <jamie.perkins@gmail.com>
2014-01-13 18:32:51 -0700

    Added startVal param, ability to count up or down, toggle easing
    - added startVal param
    - automatically detects which direction to count
    - toggle easing within the countUp object (not a param)

--- countUp.js
+++ countUp.js
@@ -5,58 +5,85 @@ window.requestAnimationFrame =
     window.msRequestAnimationFrame;
 
 // target = id of Html element where counting occurs
+// startVal = the value you want to begin at
 // endVal = the value you want to arrive at
 // decimals = number of decimal places in number, default 0
 // duration = duration in seconds, default 2
 
-function countUp(target, endVal, decimals, duration) {
+function countUp(target, startVal, endVal, decimals, duration) {
     
     var self = this;
     this.d = document.getElementById(target);
-
+    
+    startVal = Number(startVal);
+    endVal = Number(endVal);
+    this.countDown = (startVal > endVal) ? true : false;
+    
+    // toggle easing
+    this.useEasing = true;
+    
     decimals = Math.max(0, decimals || 0);
     this.dec = Math.pow(10, decimals);
     this.duration = duration * 1000 || 2000;
 
     this.startTime = null;
-    this.frameVal = 0;
-
-    endVal = Number(endVal);
+    this.frameVal = startVal;
     
     // Robert Penner's easeOutExpo
     this.easeOutExpo = function(t, b, c, d) {
-        // modified from:
         // return c * (-Math.pow(2, -10 * t / d) + 1) + b;
-        // thanks to @lifthrasiir's "exact easing" commit
         return c * (-Math.pow(2, -10 * t / d) + 1) * 1024 / 1023 + b;
+       
     }
-    this.stepUp = function(timestamp) {
+    this.count = function(timestamp) {
         
         if (self.startTime === null) self.startTime = timestamp;
      
         var progress = timestamp - self.startTime;
         
-        // easing
-        self.frameVal = self.easeOutExpo(progress, 0, endVal, self.duration);
+        // to ease or not to ease
+        if (self.useEasing) {
+            if (self.countDown) {
+                var i = self.easeOutExpo(progress, 0, startVal - endVal, self.duration);
+                self.frameVal = startVal - i;
+            } else {
+                self.frameVal = self.easeOutExpo(progress, startVal, endVal - startVal, self.duration);
+            }
+            console.log(self.frameVal);
+        } else {
+            if (self.countDown) {
+                var i = (startVal - endVal) * (progress / self.duration);
+                self.frameVal = startVal - i;
+            } else {
+                self.frameVal = startVal + (endVal - startVal) * (progress / self.duration);
+            }
+        }
         
         // decimal
-        if (self.dec > 0) {
-            self.frameVal = Math.round(self.frameVal*self.dec)/self.dec;
-            self.frameVal = (self.frameVal > endVal) ? endVal : self.frameVal;
-        } 
+        self.frameVal = Math.round(self.frameVal*self.dec)/self.dec;
         
+        // don't go past endVal since progress can exceed duration in the last frame
+        if (self.countDown) {
+            self.frameVal = (self.frameVal < endVal) ? endVal : self.frameVal;
+        } else {
+            self.frameVal = (self.frameVal > endVal) ? endVal : self.frameVal;
+        }
+        
+        // format and print value
         self.d.innerHTML = self.addCommas(self.frameVal.toFixed(decimals));
-                
+               
+        // whether to continue
         if (progress < self.duration) {
-            requestAnimationFrame(self.stepUp);
+            requestAnimationFrame(self.count);
         } 
     }  
     this.start = function() {
-        // make sure endVal is a number
-        if (!isNaN(endVal) && endVal !== null) {
-            requestAnimationFrame(self.stepUp);
+
+        // make sure values are valid
+        if (!isNaN(endVal) && !isNaN(startVal)) {
+            requestAnimationFrame(self.count);
         } else {
-            console.log('countUp error: endVal is not a number');
+            console.log('countUp error: startVal or endVal is not a number');
             self.d.innerHTML = '--';
         }
         return false;

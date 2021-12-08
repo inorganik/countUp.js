65c4436c0497ecfe3b0c0960d98b0099d8a64390
Jamie Perkins <jamie.perkins@gmail.com>
2021-12-08 06:26:09 -0700

    update packages

--- dist/countUp.umd.js
+++ dist/countUp.umd.js
@@ -2,7 +2,7 @@
     typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
     typeof define === 'function' && define.amd ? define(['exports'], factory) :
     (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.countUp = {}));
-}(this, (function (exports) { 'use strict';
+})(this, (function (exports) { 'use strict';
 
     var __assign = (undefined && undefined.__assign) || function () {
         __assign = Object.assign || function(t) {
@@ -256,4 +256,4 @@
 
     Object.defineProperty(exports, '__esModule', { value: true });
 
-})));
+}));

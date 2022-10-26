b6d7f1784028b6c052cdb489491052a76e4c20b2
Jamie Perkins <jamie.perkins@gmail.com>
2022-10-26 09:49:13 -0600

    add test, update tests

--- src/countUp.spec.ts
+++ src/countUp.spec.ts
@@ -5,6 +5,7 @@ describe('CountUp', () => {
   let countUp;
   let time;
 
+  const getTargetHtml = () => document.getElementById('target')?.innerHTML;
   const resetRAF = () => {
     time = 0;
     jest.spyOn(window, 'requestAnimationFrame').mockImplementation(cb => {
@@ -30,7 +31,7 @@ describe('CountUp', () => {
     it('should create for a valid target, and print startVal', () => {
       expect(countUp).toBeTruthy();
       expect(countUp.error.length).toBe(0);
-      expect(document.getElementById('target').innerHTML).toEqual('0');
+      expect(getTargetHtml()).toEqual('0');
     });
 
     it('should set an error for a bad target', () => {
@@ -62,7 +63,7 @@ describe('CountUp', () => {
     it('should count when start method is called', () => {
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('100');
+      expect(getTargetHtml()).toEqual('100');
     });
 
     it('should pause when pauseResume is called', () => {
@@ -77,17 +78,17 @@ describe('CountUp', () => {
       countUp.start();
       countUp.reset();
 
-      expect(document.getElementById('target').innerHTML).toEqual('0');
+      expect(getTargetHtml()).toEqual('0');
       expect(countUp.paused).toBeTruthy();
     });
 
     it('should update when update is called', () => {
       countUp.start();
-      expect(document.getElementById('target').innerHTML).toEqual('100');
+      expect(getTargetHtml()).toEqual('100');
 
       resetRAF();
       countUp.update(200);
-      expect(document.getElementById('target').innerHTML).toEqual('200');
+      expect(getTargetHtml()).toEqual('200');
     });
 
   });
@@ -98,7 +99,7 @@ describe('CountUp', () => {
       const spy = jest.spyOn(countUp, 'determineDirectionAndSmartEasing');
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('6,000');
+      expect(getTargetHtml()).toEqual('6,000');
       expect(spy).toHaveBeenCalled();
     });
 
@@ -107,37 +108,37 @@ describe('CountUp', () => {
       const spy = jest.spyOn(countUp, 'easingFn');
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('6,000');
+      expect(getTargetHtml()).toEqual('6,000');
       expect(spy).toHaveBeenCalledTimes(0);
     });
 
     it('should count down when endVal is less than startVal', () => {
       countUp = new CountUp('target', 10, { startVal: 500 });
-      expect(document.getElementById('target').innerHTML).toEqual('500');
+      expect(getTargetHtml()).toEqual('500');
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('10');
+      expect(getTargetHtml()).toEqual('10');
     });
 
     it('should handle negative numbers', () => {
       countUp = new CountUp('target', -500);
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('-500');
+      expect(getTargetHtml()).toEqual('-500');
     });
 
     it('should properly handle a zero duration', () => {
       countUp = new CountUp('target', 2000, { duration: 0 });
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('2,000');
+      expect(getTargetHtml()).toEqual('2,000');
     });
 
     it('should call the callback when finished if there is one', () => {
       const cb = jest.fn();
       countUp.start(cb);
 
-      expect(document.getElementById('target').innerHTML).toEqual('100');
+      expect(getTargetHtml()).toEqual('100');
       expect(cb).toHaveBeenCalled();
     });
   });
@@ -147,49 +148,61 @@ describe('CountUp', () => {
       countUp = new CountUp('target', 100, { decimalPlaces: 2 });
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('100.00');
+      expect(getTargetHtml()).toEqual('100.00');
     });
 
     it('should respect the duration option', () => {
       countUp = new CountUp('target', 100, { duration: 1 });
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('100');
+      expect(getTargetHtml()).toEqual('100');
     });
 
     it('should respect the useEasing option', () => {
       countUp = new CountUp('target', 100, { useEasing: false });
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('100');
+      expect(getTargetHtml()).toEqual('100');
     });
 
     it('should respect the useGrouping option', () => {
-      countUp = new CountUp('target', 10000, { useGrouping: false });
+      countUp = new CountUp('target', 100000, { useGrouping: false });
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('10000');
+      expect(getTargetHtml()).toEqual('100000');
+      resetRAF();
+
+      countUp = new CountUp('target', 1000000, { useGrouping: true });
+      countUp.start();
+
+      expect(getTargetHtml()).toEqual('1,000,000');
+    });
+
+    it('should respect the useIndianSeparators option', () => {
+      countUp = new CountUp('target', 100000, { useIndianSeparators: true });
+      countUp.start();
+
+      expect(getTargetHtml()).toEqual('1,00,000');
+      resetRAF();
+
+      countUp = new CountUp('target', 10000000, { useIndianSeparators: true });
+      countUp.start();
+
+      expect(getTargetHtml()).toEqual('1,00,00,000');
     });
 
     it('should respect the separator option', () => {
       countUp = new CountUp('target', 10000, { separator: ':' });
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('10:000');
-    });
-
-    it('should respect the separator option', () => {
-      countUp = new CountUp('target', 10000, { separator: ':' });
-      countUp.start();
-
-      expect(document.getElementById('target').innerHTML).toEqual('10:000');
+      expect(getTargetHtml()).toEqual('10:000');
     });
 
     it('should respect the decimal option', () => {
       countUp = new CountUp('target', 100, { decimal: ',', decimalPlaces: 1 });
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('100,0');
+      expect(getTargetHtml()).toEqual('100,0');
     });
 
     it('should respect the easingFn option', () => {
@@ -198,7 +211,7 @@ describe('CountUp', () => {
       countUp.start();
 
       expect(easeOutQuintic).toHaveBeenCalled();
-      expect(document.getElementById('target').innerHTML).toEqual('100');
+      expect(getTargetHtml()).toEqual('100');
     });
 
     it('should respect the formattingFn option', () => {
@@ -207,21 +220,21 @@ describe('CountUp', () => {
       countUp.start();
 
       expect(formatter).toHaveBeenCalled();
-      expect(document.getElementById('target').innerHTML).toEqual('~100~');
+      expect(getTargetHtml()).toEqual('~100~');
     });
 
     it('should respect the prefix option', () => {
       countUp = new CountUp('target', 100, { prefix: '$' });
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('$100');
+      expect(getTargetHtml()).toEqual('$100');
     });
 
     it('should respect the suffix option', () => {
       countUp = new CountUp('target', 100, { suffix: '!' });
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('100!');
+      expect(getTargetHtml()).toEqual('100!');
     });
 
     it('should respect the numerals option', () => {
@@ -229,7 +242,7 @@ describe('CountUp', () => {
       countUp = new CountUp('target', 100, { numerals });
       countUp.start();
 
-      expect(document.getElementById('target').innerHTML).toEqual('!))');
+      expect(getTargetHtml()).toEqual('!))');
     });
   });
 });

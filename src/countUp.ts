905c165c30c242512776e282a382c323367be20c
msoler75 <msoler75@yahoo.es>
2023-03-08 11:27:23 -0300

    restored original format code syntax

--- src/countUp.ts
+++ src/countUp.ts
@@ -18,7 +18,7 @@ export interface CountUpOptions { // (default)
   enableScrollSpy?: boolean; // start animation when target is in view
   scrollSpyDelay?: number; // delay (ms) after target comes into view
   scrollSpyOnce?: boolean; // run only once
-  onCompleteCallback?: () => any; // gets called when animation completes,
+  onCompleteCallback?: () => any; // gets called when animation completes
   flaps?: boolean; // digits are flaps thanks to Marcel Soler
   flapDuration?: number; // flap animation in seconds,
   flapDelay?: number; // delay last digit in animation, in seconds, 0 to deactivate
@@ -26,6 +26,7 @@ export interface CountUpOptions { // (default)
 
 // playground: stackblitz.com/edit/countup-typescript
 export class CountUp {
+
   version = '2.5.0';
   private defaults: CountUpOptions = {
     startVal: 0,
@@ -72,14 +73,12 @@ export class CountUp {
   ) {
     this.options = {
       ...this.defaults,
-      ...options,
+      ...options
     };
-    this.formattingFn = this.options.formattingFn
-      ? this.options.formattingFn
-      : this.formatNumber;
-    this.easingFn = this.options.easingFn
-      ? this.options.easingFn
-      : this.easeOutExpo;
+    this.formattingFn = (this.options.formattingFn) ?
+      this.options.formattingFn : this.formatNumber;
+    this.easingFn = (this.options.easingFn) ?
+      this.options.easingFn : this.easeOutExpo;
 
     this.startVal = this.validateValue(this.options.startVal);
     this.frameVal = this.startVal;
@@ -91,8 +90,7 @@ export class CountUp {
     if (this.options.separator === '') {
       this.options.useGrouping = false;
     }
-    this.el =
-      typeof target === 'string' ? document.getElementById(target) : target;
+    this.el = (typeof target === 'string') ? document.getElementById(target) : target;
     if (this.el) {
       this.printValue(this.startVal);
     } else {
@@ -117,23 +115,20 @@ export class CountUp {
 
   handleScroll(self: CountUp): void {
     if (!self || !window || self.once) return;
-    const bottomOfScroll = window.innerHeight + window.scrollY;
+    const bottomOfScroll = window.innerHeight +  window.scrollY;
     const rect = self.el.getBoundingClientRect();
-    const topOfEl = rect.top + window.pageYOffset;
+    const topOfEl = rect.top + window.pageYOffset
     const bottomOfEl = rect.top + rect.height + window.pageYOffset;
-    if (
-      bottomOfEl < bottomOfScroll &&
-      bottomOfEl > window.scrollY &&
-      self.paused
-    ) {
+    if (bottomOfEl < bottomOfScroll && bottomOfEl >  window.scrollY && self.paused) {
       // in view
       self.paused = false;
       setTimeout(() => self.start(), self.options.scrollSpyDelay);
-      if (self.options.scrollSpyOnce) self.once = true;
+      if (self.options.scrollSpyOnce)
+        self.once = true;
     } else if (
-      (window.scrollY > bottomOfEl || topOfEl > bottomOfScroll) &&
-      !self.paused
-    ) {
+        (window.scrollY > bottomOfEl || topOfEl > bottomOfScroll) &&
+        !self.paused
+      ) {
       // out of view
       self.reset();
     }
@@ -146,16 +141,13 @@ export class CountUp {
    * usingEasing is true and the total animation amount exceeds the smartEasingThreshold.
    */
   private determineDirectionAndSmartEasing(): void {
-    const end = this.finalEndVal ? this.finalEndVal : this.endVal;
-    this.countDown = this.startVal > end;
+    const end = (this.finalEndVal) ? this.finalEndVal : this.endVal;
+    this.countDown = (this.startVal > end);
     const animateAmount = end - this.startVal;
-    if (
-      Math.abs(animateAmount) > this.options.smartEasingThreshold &&
-      this.options.useEasing
-    ) {
+    if (Math.abs(animateAmount) > this.options.smartEasingThreshold && this.options.useEasing) {
       this.finalEndVal = end;
-      const up = this.countDown ? 1 : -1;
-      this.endVal = end + up * this.options.smartEasingAmount;
+      const up = (this.countDown) ? 1 : -1;
+      this.endVal = end + (up * this.options.smartEasingAmount);
       this.duration = this.duration / 2;
     } else {
       this.endVal = end;
@@ -228,9 +220,7 @@ export class CountUp {
   }
 
   count = (timestamp: number): void => {
-    if (!this.startTime) {
-      this.startTime = timestamp;
-    }
+    if (!this.startTime) { this.startTime = timestamp; }
 
     const progress = timestamp - this.startTime;
     this.remaining = this.duration - progress;
@@ -238,32 +228,16 @@ export class CountUp {
     // to ease or not to ease
     if (this.useEasing) {
       if (this.countDown) {
-        this.frameVal =
-          this.startVal -
-          this.easingFn(
-            progress,
-            0,
-            this.startVal - this.endVal,
-            this.duration
-          );
+        this.frameVal = this.startVal - this.easingFn(progress, 0, this.startVal - this.endVal, this.duration);
       } else {
-        this.frameVal = this.easingFn(
-          progress,
-          this.startVal,
-          this.endVal - this.startVal,
-          this.duration
-        );
+        this.frameVal = this.easingFn(progress, this.startVal, this.endVal - this.startVal, this.duration);
       }
     } else {
-      this.frameVal =
-        this.startVal +
-        (this.endVal - this.startVal) * (progress / this.duration);
+      this.frameVal = this.startVal + (this.endVal - this.startVal) * (progress / this.duration);
     }
 
     // don't go past endVal since progress can exceed duration in the last frame
-    const wentPast = this.countDown
-      ? this.frameVal < this.endVal
-      : this.frameVal > this.endVal;
+    const wentPast = this.countDown ? this.frameVal < this.endVal : this.frameVal > this.endVal;
     this.frameVal = wentPast ? this.endVal : this.frameVal;
 
     // decimal
@@ -283,7 +257,7 @@ export class CountUp {
         this.options.onCompleteCallback();
       }
     }
-  };
+  }
 
   // Marcel Soler
   printFlaps(result: string) {
@@ -427,7 +401,7 @@ export class CountUp {
       }
     }
   }
-
+  
   printValue(val: number): void {
     const result = this.formattingFn(val);
     if (!this.el) return;
@@ -435,7 +409,6 @@ export class CountUp {
       this.printFlaps(result);
       return;
     }
-    console.log('normal');
     if (this.el.tagName === 'INPUT') {
       const input = this.el as HTMLInputElement;
       input.value = result;
@@ -447,7 +420,7 @@ export class CountUp {
   }
 
   ensureNumber(n: any): boolean {
-    return typeof n === 'number' && !isNaN(n);
+    return (typeof n === 'number' && !isNaN(n));
   }
 
   validateValue(value: string | number): number {
@@ -469,7 +442,7 @@ export class CountUp {
   // default format and easing functions
 
   formatNumber = (num: number): string => {
-    const neg = num < 0 ? '-' : '';
+    const neg = (num < 0) ? '-' : '';
     let result: string, x1: string, x2: string, x3: string;
     result = Math.abs(num).toFixed(this.options.decimalPlaces);
     result += '';
@@ -478,14 +451,13 @@ export class CountUp {
     x2 = x.length > 1 ? this.options.decimal + x[1] : '';
     if (this.options.useGrouping) {
       x3 = '';
-      let factor = 3,
-        j = 0;
+      let factor = 3, j = 0;
       for (let i = 0, len = x1.length; i < len; ++i) {
         if (this.options.useIndianSeparators && i === 4) {
           factor = 2;
           j = 1;
         }
-        if (i !== 0 && j % factor === 0) {
+        if (i !== 0 && (j % factor) === 0) {
           x3 = this.options.separator + x3;
         }
         j++;
@@ -499,9 +471,10 @@ export class CountUp {
       x2 = x2.replace(/[0-9]/g, (w) => this.options.numerals[+w]);
     }
     return neg + this.options.prefix + x1 + x2 + this.options.suffix;
-  };
+  }
 
   // t: current time, b: beginning value, c: change in value, d: duration
   easeOutExpo = (t: number, b: number, c: number, d: number): number =>
-    (c * (-Math.pow(2, (-10 * t) / d) + 1) * 1024) / 1023 + b;
+    c * (-Math.pow(2, -10 * t / d) + 1) * 1024 / 1023 + b;
+
 }
